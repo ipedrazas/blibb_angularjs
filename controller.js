0f23c52f2b01dceae295a329a8f6a5218213f63c
Ivan Pedrazas <ipedrazas@gmail.com>
2013-06-13 00:44:12 +0100

    clean up and readme added

--- controller.js
+++ controller.js
@@ -1,38 +1,4 @@
-// var VideoController = function ($scope, $http) {
 
-//     var serviceUrl = "/api/videos";
-
-//     var blankVideo = {
-//         Id: "",
-//         Title: "",
-//         Length: 75
-//     };
-
-//     var refreshVideos = function() {
-//         $http.get(serviceUrl).success(function (data) {
-//             $scope.videos = data;
-//         });
-//     };
-
-//     $scope.isEditVisible = false;
-
-//     $scope.showEdit = function () {
-//         $scope.isEditVisible = true;
-//         $scope.editableVideo = angular.copy(blankVideo);
-//     };
-
-//     $scope.saveVideo = function() {
-//         $scope.isEditVisible = false;
-//         $http.post(serviceUrl, $scope.editableVideo)
-//              .success(function (video) {
-//                  $scope.videos.push(video);
-//              });
-//     };
-
-//     refreshVideos();
-// };
-
-//resp.setHeader("Access-Control-Allow-Headers", req.getHeader("Access-Control-Request-Headers"));
 var EntryController = function ($scope, $http) {
 
     var serviceUrl = "http://api.oioi.me/ipedrazas/service-for-angular-js";
@@ -56,7 +22,9 @@ var EntryController = function ($scope, $http) {
     $scope.saveEntry = function() {
         $scope.isEditVisible = false;
         $scope.entry.app_token = "service-for-angular-js";
-        // console.log($scope.entry);
+        if( console && console.log ) {
+            console.log($scope.entry);
+        }
 
         //
         //  This should work but gives a CORS error
@@ -67,13 +35,13 @@ var EntryController = function ($scope, $http) {
         //      });
 
         $.ajax({
-          type: "POST",
-          url: serviceUrl,
-          data: $scope.entry
+            type: "POST",
+            url: serviceUrl,
+            data: $scope.entry
         }).done(function ( data ) {
               $scope.entries.push($scope.entry);
+        });
 
-            });
         refreshEntries();
     };
 
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Angular.js with :blibb
+
+
+This is a little example of a mini app written in angular.js that consumes :blibb services.
+
+It is a very minimal application with a table with 2 columns that are populated from the API that generates :blibb for that collection. There is a form that sends data to the server using jQuery since the Angular.js `$http.post` throws a CORS error.
